7439a767f47aa60122afaa638acea9abd4a2f42a
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-20 13:29:09 +0200

    Edited headers and titles.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
-      <p>We will be friends soon!</p>
+      <h1>Practice Session 20.02</h1>
+      <p>Here we come!</p>
     </div>
   );
 }
