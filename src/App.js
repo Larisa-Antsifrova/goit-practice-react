01aa5db7b4a2c0feb670e387e056ea1300227eda
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-20 13:27:09 +0200

    Added header.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
+      <h1>React. Intro lecture.</h1>
       <p>We will be friends soon!</p>
     </div>
   );
