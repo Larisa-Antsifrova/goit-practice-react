50ee4c9d7ef55fc64dcee842614e6fb570bd8163
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-20 13:15:47 +0200

    Prepared initial project files.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      <p>Will we be friends?</p>
+    </div>
+  );
+}
+
+export default App;
