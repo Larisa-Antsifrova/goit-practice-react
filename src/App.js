fcf6856586046bc0fd006533eebfc6154aefcd2e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-03 00:50:23 +0200

    Added basic ToDoList component and styles.

--- src/App.js
+++ src/App.js
@@ -10,14 +10,28 @@ import todos from './db/todos.json';
 class App extends Component {
   state = { todos: todos };
 
+  deleteToDo = toDoId => {
+    this.setState(prevState => {
+      return {
+        todos: prevState.todos.filter(todo => todo.id !== toDoId),
+      };
+    });
+  };
+
   render() {
     const { todos } = this.state;
-    console.log(todos);
+    const totalToDoCount = todos.length;
+    const completedToDoCount = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0,
+    );
 
     return (
       <>
         <h2>To Do List</h2>
-        <ToDoList todos={todos} />
+        <p className="temporary-p">Total: {totalToDoCount}</p>
+        <p className="temporary-p">Completed: {completedToDoCount}</p>
+        <ToDoList todos={todos} onDeleteToDo={this.deleteToDo} />
 
         <h2>Counter</h2>
         <Counter />
@@ -33,23 +47,3 @@ class App extends Component {
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <>
-//       <h2>To Do List</h2>
-//       <ToDoList />
-
-//       <h2>Counter</h2>
-//       <Counter />
-
-//       <h2>Dropdown</h2>
-//       <Dropdown />
-
-//       <h2>Color Picker</h2>
-//       <ColorPicker options={options} />
-//     </>
-//   );
-// }
-
-// export default App;
--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import styles from './ToDoList.module.css';
 
-const ToDoList = ({ todos }) => (
+const ToDoList = ({ todos, onDeleteToDo }) => (
   <ul className={styles.TodoList}>
     {todos.map(({ id, text }) => (
       <li key={id} className={styles.TodoList__item}>
         <p className={styles.TodoList__text}>{text}</p>
-        <button className={styles.btn}>Delete</button>
+        <button className={styles.btn} onClick={() => onDeleteToDo(id)}>
+          Delete
+        </button>
       </li>
     ))}
   </ul>
