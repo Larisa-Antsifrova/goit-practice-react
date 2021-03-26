f809e3e4d6b10547c42244b992ad3c5f4cf75ef8
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-27 00:36:54 +0200

    Refactored reducers with the help of combineReducers method.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,53 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const initialState = {
-  counter: {
-    value: 0,
-    step: 5,
-  },
-};
-
-const reducer = (state = initialState, { type, payload }) => {
+const valueReducer = (state = 0, { type, payload }) => {
   switch (type) {
     case 'counter/Increment':
-      return {
-        ...state,
-        counter: {
-          ...state.counter,
-          value: state.counter.value + payload,
-        },
-      };
+      return state + payload;
     case 'counter/Decrement':
-      return {
-        ...state,
-        counter: {
-          ...state.counter,
-          value:
-            state.counter.value - payload < 0
-              ? 0
-              : state.counter.value - payload,
-        },
-      };
+      return state - payload < 0 ? 0 : state - payload;
     default:
       return state;
   }
 };
 
-const store = createStore(reducer);
+const stepReducer = (state = 5, action) => state;
+
+const counterReducer = combineReducers({
+  value: valueReducer,
+  step: stepReducer,
+});
+
+const rootReducer = combineReducers({ counter: counterReducer });
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
+
+// Another heavier variant withouth combining reducers
+// const reducer = (state = initialState, { type, payload }) => {
+//   switch (type) {
+//     case 'counter/Increment':
+//       return {
+//         ...state,
+//         counter: {
+//           ...state.counter,
+//           value: state.counter.value + payload,
+//         },
+//       };
+//     case 'counter/Decrement':
+//       return {
+//         ...state,
+//         counter: {
+//           ...state.counter,
+//           value:
+//             state.counter.value - payload < 0
+//               ? 0
+//               : state.counter.value - payload,
+//         },
+//       };
+//     default:
+//       return state;
+//   }
+// };
