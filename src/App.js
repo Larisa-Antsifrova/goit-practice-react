24725026276ab013d2e5af2cd52f15b27eb352b2
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-26 22:42:57 +0200

    Added actions and reducer for Counter.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Counter from './components/Counter';
 
+import store from './redux/store';
+import { myAction, myAction2 } from './redux/actions';
+
+store.dispatch(myAction(11));
+store.dispatch(myAction2);
+
 class App extends Component {
   render() {
     return (
