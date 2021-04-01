d31f59512bc3b1c03ee70f23e37ce23ea4e56020
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-01 22:22:25 +0300

    Added input reset.

--- src/pages/CocktailsPage/CocktailsPage.js
+++ src/pages/CocktailsPage/CocktailsPage.js
@@ -3,11 +3,29 @@ import { getCocktails } from '../../services/cocktailsApi';
 class CocktailsPage extends Component {
   state = {
     drinks: [],
+    query: '',
   };
 
   componentDidMount() {
     getCocktails('margarita').then(drinks => this.setState({ drinks }));
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query } = this.state;
+
+    if (prevState.query !== this.state.query) {
+      getCocktails(query).then(drinks => this.setState({ drinks }));
+    }
+  }
+
+  SubmitHandler = event => {
+    let { query } = event.target;
+    event.preventDefault();
+
+    this.setState({ query: query.value });
+
+    query.value = '';
+  };
 
   render() {
     const { drinks } = this.state;
@@ -15,11 +33,18 @@ class CocktailsPage extends Component {
     return (
       <>
         <h1>Cocktails Page</h1>
-        <ul>
-          {drinks.map(drink => (
-            <li key={drink.idDrink}>{drink.strDrink}</li>
-          ))}
-        </ul>
+        <form onSubmit={this.SubmitHandler}>
+          <input type="text" name="query" placeholder="Maybe, a drink?" />
+        </form>
+        {drinks ? (
+          <ul>
+            {drinks.map(drink => (
+              <li key={drink.idDrink}>{drink.strDrink}</li>
+            ))}
+          </ul>
+        ) : (
+          'No drinks found!'
+        )}
       </>
     );
   }
