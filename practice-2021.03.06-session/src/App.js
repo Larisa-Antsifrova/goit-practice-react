4918199cefc233984efcaa92251a290424fa1e1e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-05 23:02:49 +0300

    Grouped project files.

---
+++ practice-2021.03.06-session/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      <p>We will be friends soon!</p>
+    </div>
+  );
+}
+
+export default App;
